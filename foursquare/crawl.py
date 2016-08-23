5bcd4fd90d41d276e558d9da4452fa15cd87e3d7
Antonio Lima <anto87@gmail.com>
2016-08-23 15:49:33 +0100

    Adding foursquare crawler.

---
+++ foursquare/crawl.py
@@ -0,0 +1,75 @@
+"""This script matches venue strings / locations to FourSquare IDs and retrieves venues details from the FourSquare API.
+
+"""
+
+import csv
+import sys
+
+import click
+import requests
+import ratelim
+import json
+
+from urllib.parse import urljoin
+
+BASE_URL = "https://api.foursquare.com/v2/"
+
+class FoursquareAPI(object):
+    search_url = urljoin(BASE_URL, "venues/search")
+    venue_url = urljoin(BASE_URL, "venues/search")
+
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def _build_arguments(self, arguments):
+        base_arguments = {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'v': '20160823',
+        }
+        base_arguments.update(arguments)
+        return base_arguments
+
+    @ratelim.patient(2000, 3600)
+    def _get(self, endpoint, arguments):
+        resp = requests.get(endpoint,
+                            params=self._build_arguments(arguments))
+        return resp
+
+    def search(self, **arguments):
+        return self._get(self.search_url, arguments)
+
+    def venue(self, venue_id):
+        return self._get(self.venue_url, arguments)
+
+@click.command()
+@click.option('--client_id', envvar="FSQ_ID")
+@click.option('--client_secret', envvar="FSQ_SECRET")
+@click.option('--infile', default=sys.stdin, type=click.File('r'))
+@click.option('--outfile', default=sys.stdout)
+def main(client_id, client_secret, infile, outfile):
+    """
+    It accepts as input a no-header csvfile of three fields: name,latitude,longitude. For example:
+
+        British Museum,44.5,0.15
+        ...
+
+    If the latitude/longitude is 0,0 it will perform a global search
+
+    """
+    api = FoursquareAPI(client_id, client_secret)
+    reader = csv.reader(infile)
+    for name, lat, lon in reader:
+        intent = 'match'
+        if lat == 0 and lon == 0:
+            intent = 'global'
+        result = api.search(query=name, intent=intent, ll="{},{}".format(lat, lon)).json()
+        if len(result['response']['venues']) == 0 and intent != "global":
+            intent = 'checkin'
+            result = api.search(query=name, intent=intent, ll="{},{}".format(lat, lon)).json()
+        print(json.dumps(result))
+
+
+if __name__ == "__main__":
+    main()
